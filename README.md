4dfb711095edd96d06d9f1795996880acc93848a
Kuba <jakub@unold.pl>
2022-02-26 22:36:08 +0100

    Tidied up project description. Preparing code for working on 4 ESP32 boards

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+This code should be used to to control 4 different ESP32 on SAE 2022
+
 ```
 ./sim_vehicle.py -v ArduPlane --out /dev/ttyUSB0,115200 -N
 ```
--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,11 @@
 /*********************************************************************************
- * ESP-Now-Serial-Bridge
- *
- * ESP32 based serial bridge for transmitting serial data between two boards
- *
- * The primary purpose of this sketch was to enable a MAVLink serial connection,
- *   which I successfully tested at 57600 bps.  In theory, much faster buad rates
- *   should work fine, but I have not tested faster than 115200.
- *
- * Range is easily better than regular WiFi, however an external antenna may be
- *   required for truly long range messaging, to combat obstacles/walls, and/or
- *   to achieve success in an area saturated with 2.4GHz traffic.
+ * Program for creating wireless serial bridge between 2 ESP32
  * 
- * I made heavy use of compiler macros to keep the sketch compact/efficient.
- *
- * To find the MAC address of each board, uncomment the #define DEBUG line, 
- *   and monitor serial output on boot.  Set the OPPOSITE board's IP address
- *   as the value for RECVR_MAC in the macros at the top of the sketch.
- *   
- * The BLINK_ON_* macros should be somewhat self-explanatory.  If your board has a built-in
- *   LED (or you choose to wire an external one), it can indicate ESP-Now activity as
- *   defined by the macros you choose to enable.
- *
- * When uploading the sketch, be sure to define BOARD1 or BOARD2 as appropriate
- *   before compiling.
- *
- * -- Yuri - Sep 2021
- *
- * Based this example - https://randomnerdtutorials.com/esp-now-two-way-communication-esp32/
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files.
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * In project 4 ESP32 boards will be used: one Mother and 3 Children
+ * 
+ * Only 1 Child is turned on, ready to communicate with Mother.
+ * Mother chooses which Child to talk to by grounding (or pulling up?)
+ * specific pin
 *********************************************************************************/
 
 #include <esp_now.h>
