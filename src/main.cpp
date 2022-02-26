78268fdf5d79618688339bba4320f5b6e6e72049
Kuba <jakub@unold.pl>
2022-02-27 00:09:35 +0100

    Should be working on 4 different boards. time to check their mac addresses.

--- src/main.cpp
+++ src/main.cpp
@@ -3,35 +3,56 @@
  * 
  * In project 4 ESP32 boards will be used: one Mother and 3 Children
  * 
- * Only 1 Child is turned on, ready to communicate with Mother.
+ * Only 1 Child (PADA) is turned on, ready to communicate with Mother (PA).
  * Mother chooses which Child to talk to by grounding (or pulling up?)
  * specific pin
+ * 
+ * Boards:
+ * 1) MOTHER
+ * 2) CHILD1
+ * 3) CHILD2
+ * 4) CHILD3
+ * 
+ * If you want Mother to send to:
+ *      CHILD1, then ground GPIO5
+ *      CHILD2, then ground GPIO6
+ *      CHILD3, then ground GPIO7
 *********************************************************************************/
 
+#include "Arduino.h"
 #include <esp_now.h>
 #include <WiFi.h>
 #include <esp_wifi.h>
 
-/** 
- * BOARD1 is the one with white paper attached
- * BOARD2 looks better and has small blue dot on an antenna mounted on the board
-*/
+/** Uncomment only one! */
+// #define MOTHER
+#define CHILD1
+// #define CHILD2
+// #define CHILD3
 
-#define BOARD2 // BOARD1 or BOARD2
-
-#ifdef BOARD1
-#define RECVR_MAC {0x3C, 0x71, 0xBF, 0x4B, 0xF2, 0x1C}  // replace with your board's address
-//#define BLINK_ON_SEND
-//#define BLINK_ON_SEND_SUCCESS
-#define BLINK_ON_RECV
-#else
-#define RECVR_MAC {0x94, 0xB9, 0x7E, 0xFA, 0xD0, 0x10}  // replace with your board's address
-//#define BLINK_ON_SEND
-// #define BLINK_ON_SEND_SUCCESS
-//#define BLINK_ON_RECV
+/** if I am the Mother */
+#ifdef MOTHER
+    #define CHILD1_PIN 5
+    #define CHILD2_PIN 6
+    #define CHILD3_PIN 7
+    #define CHILD1_RECVR_MAC {0x94, 0xB9, 0x7E, 0xFA, 0xD0, 0x10}
+    #define CHILD2_RECVR_MAC {0xff, 0xff, 0xff, 0xff, 0xff, 0x00}
+    #define CHILD3_RECVR_MAC {0xff, 0xff, 0xff, 0xff, 0xff, 0x00}
+    const uint8_t child1_address[] = CHILD1_RECVR_MAC;
+    const uint8_t child2_address[] = CHILD2_RECVR_MAC;
+    const uint8_t child3_address[] = CHILD3_RECVR_MAC;
 #endif
 
+/** if I am one of the Chilren */
+#ifndef MOTHER
+    #define MOTHER_RECVR_MAC {0x94, 0xB9, 0x7E, 0xFA, 0xD0, 0x10}
+    const uint8_t mother_address[] = MOTHER_RECVR_MAC;
+#endif
+
+#define SHOW_MAC_ADDRESS
+
 #define WIFI_CHAN  13 // 12-13 only legal in US in lower power mode, do not use 14
+
 /** Perhaps I can live baudrate at 9600 for slick printing but
  * when running simulation I should use higher speed in order to 
  * meet minimum requirements when it comes to amount of data ArduPilot
@@ -39,18 +60,13 @@
  */
 #define BAUD_RATE  115200
 // #define BAUD_RATE  9600  #up to 1200Bps
+
 #define TX_PIN     1 // default UART0 is pin 1 (shared by USB)
 #define RX_PIN     3 // default UART0 is pin 3 (shared by USB)
 #define SER_PARAMS SERIAL_8N1 // SERIAL_8N1: start/stop bits, no parity
 
 #define BUFFER_SIZE 250 // max of 250 bytes
-// #define DEBUG // for additional serial messages (may interfere with other messages)
 
-#ifndef LED_BUILTIN
-#define LED_BUILTIN 2  // some boards don't have an LED or have it connected elsewhere
-#endif
-
-const uint8_t broadcastAddress[] = RECVR_MAC;
 // wait for double the time between bytes at this serial baud rate before sending a packet
 // this *should* allow for complete packet forming when using packetized serial comms
 const uint32_t timeout_micros = (int)(1.0 / BAUD_RATE * 1E6) * 20;
@@ -62,31 +78,7 @@ uint32_t send_timeout = 0;
 
 esp_now_peer_info_t peerInfo;  // scope workaround for arduino-esp32 v2.0.1
 
-#if defined(DEBUG) || defined(BLINK_ON_SEND_SUCCESS)
-uint8_t led_status = 0;
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-    #ifdef DEBUG
-    if (status == ESP_NOW_SEND_SUCCESS) {
-        Serial.println("Send success");
-    } else {
-    Serial.println("Send failed");
-    }
-    #endif
-
-    #ifdef BLINK_ON_SEND_SUCCESS
-    if (status == ESP_NOW_SEND_SUCCESS) {
-        led_status = ~led_status;
-        // this function happens too fast to register a blink
-        // instead, we latch on/off as data is successfully sent
-        digitalWrite(LED_BUILTIN, led_status);
-        return;
-    }
-    // turn off the LED if send fails
-    led_status = 0;
-    digitalWrite(LED_BUILTIN, led_status);
-    #endif
-}
-#endif
+static uint8_t broadcastAddress[6] = {};
 
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
     #ifdef BLINK_ON_RECV
@@ -105,19 +97,49 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
     Serial.println(len);
     #endif
 }
- 
-void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
+
+
+void setup()
+{
+    /* I am the mother*/
+    #ifdef MOTHER
+    pinMode(5, INPUT_PULLUP);
+    pinMode(6, INPUT_PULLUP);
+    pinMode(7, INPUT_PULLUP);
+
+    if (digitalRead(CHILD1_PIN) == LOW)
+    {   // child one has been chosen
+        memcpy(broadcastAddress, child1_address, sizeof(child1_address));
+    }
+    else if (digitalRead(CHILD2_PIN) == LOW)
+    {   // child two has been chosen
+        memcpy(broadcastAddress, child2_address, sizeof(child2_address));
+    }
+    else if (digitalRead(CHILD3_PIN) == LOW)
+    {   // child two has been chosen
+        memcpy(broadcastAddress, child3_address, sizeof(child3_address));
+    }
+    else
+    {
+        Serial.println("Error: None of the pins is grounded!");
+    }
+    #endif
+
+    /* I am one of the Children*/
+    #ifndef MOTHER
+    memcpy(broadcastAddress, mother_address, sizeof(mother_address));
+    #endif
+
     Serial.begin(BAUD_RATE, SER_PARAMS, RX_PIN, TX_PIN);
     Serial.println(send_timeout);
     WiFi.mode(WIFI_STA);
 
-    #ifdef DEBUG
+    #ifdef SHOW_MAC_ADDRESS
     Serial.print("ESP32 MAC Address: ");
     Serial.println(WiFi.macAddress());
     #endif
-    
-    if (esp_wifi_set_channel(WIFI_CHAN, WIFI_SECOND_CHAN_NONE) != ESP_OK) {
+
+        if (esp_wifi_set_channel(WIFI_CHAN, WIFI_SECOND_CHAN_NONE) != ESP_OK) {
         #ifdef DEBUG
         Serial.println("Error changing WiFi channel");
         #endif
@@ -135,7 +157,6 @@ void setup() {
     esp_now_register_send_cb(OnDataSent);
     #endif
     
-    // esp_now_peer_info_t peerInfo;  // scope workaround for arduino-esp32 v2.0.1
     memcpy(peerInfo.peer_addr, broadcastAddress, 6);
     peerInfo.channel = WIFI_CHAN;  
     peerInfo.encrypt = false;
@@ -150,11 +171,13 @@ void setup() {
     esp_now_register_recv_cb(OnDataRecv);
 }
 
-void loop() {
-
+void loop()
+{
     // read up to BUFFER_SIZE from serial port
-    if (Serial.available()) {
-        while (Serial.available() && buf_size < BUFFER_SIZE) {
+    if (Serial.available())
+    {
+        while (Serial.available() && buf_size < BUFFER_SIZE)
+        {
             buf_send[buf_size] = Serial.read();
             send_timeout = micros() + timeout_micros;
             buf_size++;
@@ -162,23 +185,9 @@ void loop() {
     }
 
     // send buffer contents when full or timeout has elapsed
-    if (buf_size == BUFFER_SIZE || (buf_size > 0 && micros() >= send_timeout)) {
-        #ifdef BLINK_ON_SEND
-        digitalWrite(LED_BUILTIN, HIGH);
-        #endif
+    if (buf_size == BUFFER_SIZE || (buf_size > 0 && micros() >= send_timeout))
+    {
         esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &buf_send, buf_size);
         buf_size = 0;
-        #ifdef DEBUG
-        if (result == ESP_OK) {
-            Serial.println("Sent!");
-        }
-        else {
-            Serial.println("Send error");
-        }
-        #endif
-        #ifdef BLINK_ON_SEND
-        digitalWrite(LED_BUILTIN, LOW);
-        #endif
     }
-
 }
